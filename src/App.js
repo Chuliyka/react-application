754d2244b8426eaf9182d5f92c4cd9139b6d9476
Chuliyka <116159308+Chuliyka@users.noreply.github.com>
2022-11-11 12:26:23 +0200

    create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/navigation';
-import Content from './components/content';
+
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Content />
+    
       </div>
      
   );
